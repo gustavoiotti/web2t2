489a28ab2bc00e5ba0feb80a2223d05359f173af
Gustavo Iotti <gustavo.iotti@hotmail.com>
2019-07-08 16:16:06 -0300

    cm2

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -4,6 +4,9 @@ const crypto = require("crypto");
 const aws = require("aws-sdk");
 const multerS3 = require("multer-s3");
 
+var VIDEO_TYPES = ['video/mp4', 'video/webm', 'video/ogg', 'video/ogv'];
+
+
 const storageTypes = {
   local: multer.diskStorage({
     destination: (req, file, cb) => {
@@ -38,16 +41,19 @@ const storageTypes = {
 
 module.exports = {
   dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-  storage: storageTypes[process.env.STORAGE_TYPE],
+  //storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
-    fileSize: 2 * 1024 * 1024
+    fileSize: 200 * 1024 * 1024
+    //fileSize: 100000000000000000000
   },
   fileFilter: (req, file, cb) => {
     const allowedMimes = [
-      "image/jpeg",
-      "image/pjpeg",
-      "image/png",
-      "image/gif"
+      "video/jpeg",
+      "video/pjpeg",
+      "video/png",
+      "video/gif",
+      "video/mp4"
+      
     ];
 
     if (allowedMimes.includes(file.mimetype)) {
@@ -57,3 +63,58 @@ module.exports = {
     }
   }
 };
+
+/*exports.uploadVideo = function(req, res) {
+  var src;
+  var dest;
+  var targetPath;
+
+  console.log(req);
+
+  var tempPath = req.file.path;
+  var type = mime.lookup(req.file.mimetype);
+
+  if (VIDEO_TYPES.indexOf(type) == -1) {
+      return res.status(415).send('Supported video formats: mp4, webm, ogg, ogv');
+  }
+
+  targetPath = './public/videos/' + req.file.originalname;
+  
+  src = fs.createReadStream(tempPath);
+  dest = fs.createWriteStream(targetPath);
+  src.pipe(dest);
+
+  src.on('error', function(error) {
+      if (error) {
+          return res.status(500).send({
+              message: error
+          });
+      }
+  });
+
+  src.on('end', function() {
+      var video = new Videos(req.body);
+      video.videoName = req.file.originalname;
+      video.user = req.user;
+
+      video.save(function(error) {
+          if (error) {
+              return res.status(400).send({
+                  message: error
+              });
+          }
+      });
+
+      fs.unlink(tempPath, function(err) {
+          if (err) {
+              return res.status(500).send({
+                  message: error
+              });
+          }
+
+          res.redirect('videos');
+
+      });
+  });
+};
+*/
